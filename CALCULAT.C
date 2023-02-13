1856721728d6e3d19fda6de48e7e911436bf92be
janu149 <125281797+janu149@users.noreply.github.com>
2023-02-13 20:36:57 +0530

    Add files via upload

---
+++ CALCULAT.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("enter the value!");
+scanf("%d%d",&a,&b);
+printf("addition %d",a+b);
+printf("\n subraction %d",a-b);
+printf("\multiplication %d",a*b);
+printf("\n division %d");
+getch();
+}
