98f31471f54b372615e2169cdd811ada6e85c5d5
janu149 <125281797+janu149@users.noreply.github.com>
2023-02-13 20:45:19 +0530

    Add files via upload

---
+++ MERGE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int arr1[50], arr2[50], size1, size2, i, k, merge[100];
+  print("Enter array 1 size:");
+  scanf("%d", &size1);
+  printf("Enter array 1 element: ");
+  for(i=0; i<size1; i++)
+  {
+    scanf("%d", &arr1[i]);
+    merge[i] = array1[i];
+  }
+  k=i;
+  printf(
